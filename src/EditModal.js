12574d46d235f3cb987fd54b086bfbad7997db86
Sara L <saraningliu@gmail.com>
2019-10-25 22:50:55 -0400

    Styled modal. Added basic form to modal. Installed react-native-simple-radio-button.

--- src/EditModal.js
+++ src/EditModal.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import {Modal, View, Text} from 'react-native';
 import Button from "react-native-button";
+import {Dimensions} from "react-native";
+import {Switch, Picker, TextInput} from 'react-native';
+import RadioForm from "react-native-simple-radio-button";
 
 export default class EditModal extends React.Component {
 
@@ -12,16 +15,43 @@ export default class EditModal extends React.Component {
         return (
             <View>
                 <Modal animationType="slide"
-                       presentationStyle={'pageSheet'}
-                       transparent={false}
+                       transparent={true}
                        visible={this.props.visible}
                        onRequestClose={this.props.close}>
-                    <View style={{flex:1, flexDirection:'column'}}>
-                        <Text style={{flex:3}}> Hi i am a modal screen!! </Text>
-                    <View style={{flex:1, flexDirection:'row', alignItems:'flex-end', justifyContent:'center'}}>
-                        <Button style={{fontSize:40}} > ✓ </Button>
-                        <Button style={{fontSize:40}} onPress={this.props.close}> X </Button>
-                    </View>
+                    <View style={{flex: 1, backgroundColor: 'rgba(80,80,80,0.5)'}}>
+                        <View style={{
+                            backgroundColor: '#fff', padding: 20, width: Dimensions.get('window').width * 0.9,
+                            height: Dimensions.get('window').height * 0.9
+                        }}>
+                            <Text style={{flex: 1, fontSize:30}}>Hi i am a modal screen!!</Text>
+                            <View style={{flex: 3, flexDirection:'column', justifyContent:'space-between'}}>
+                                <View style={{flexDirection:'row'}}>
+                                    <Text style={{fontSize:20}}>USER</Text>
+                                    <RadioForm initial={0} radio_props={[{label: 'user1'},{label: 'user2'}]} />
+                                </View>
+                                <View style={{flexDirection:'row'}}>
+                                    <Text style={{fontSize:20}}>ACTION</Text>
+                                    <Picker></Picker>
+                                </View>
+                                <View style={{flexDirection:'row'}}>
+                                    <Text style={{fontSize:20}}>NUMBER</Text>
+                                    <TextInput style={{backgroundColor:'pink'}}></TextInput>
+                                </View>
+                                <View style={{flexDirection:'row'}}>
+                                    <Text style={{fontSize:20}}>RANDOM</Text>
+                                    <Switch></Switch>
+                                </View>
+                            </View>
+                            <View style={{
+                                flex: 1,
+                                flexDirection: 'row',
+                                alignItems: 'flex-end',
+                                justifyContent: 'flex-end'
+                            }}>
+                                <Button style={{fontSize: 40}} onPress={this.props.close}> ✓ </Button>
+                                <Button style={{fontSize: 40}} onPress={this.props.close}> X </Button>
+                            </View>
+                        </View>
                     </View>
                 </Modal>
             </View>
