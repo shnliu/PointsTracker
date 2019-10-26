b2366c2566e22756471dcbb614cd11bb15c3f5e6
Sara L <saraningliu@gmail.com>
2019-10-26 09:22:09 -0400

    created stylesheet for edit modal.

--- src/EditModal.js
+++ src/EditModal.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Modal, View, Text} from 'react-native';
 import Button from "react-native-button";
-import {Dimensions} from "react-native";
+import {Dimensions, StyleSheet} from "react-native";
 import {Switch, Picker, TextInput} from 'react-native';
 import RadioForm from "react-native-simple-radio-button";
 
@@ -11,26 +11,39 @@ export default class EditModal extends React.Component {
         super(props);
     }
 
+    styles = StyleSheet.create({
+        pageViewStyle: {
+            flex: 1,
+            backgroundColor: 'rgba(80,80,80,0.5)'
+        },
+        modalViewStyle:{
+            backgroundColor: '#fff',
+            padding: 20,
+            width: Dimensions.get('window').width * 0.9,
+            height: Dimensions.get('window').height * 0.9
+        },
+        optionViewStyle:{
+            flexDirection: 'row',
+            alignItems:'center',
+            justifyContent:'space-between'
+        },
+
+    });
+
     render() {
         return (
             <View>
-                <Modal animationType="slide"
-                       transparent={true}
-                       visible={this.props.visible}
-                       onRequestClose={this.props.close}>
-                    <View style={{flex: 1, backgroundColor: 'rgba(80,80,80,0.5)'}}>
-                        <View style={{
-                            backgroundColor: '#fff', padding: 20, width: Dimensions.get('window').width * 0.9,
-                            height: Dimensions.get('window').height * 0.9
-                        }}>
+                <Modal animationType="slide" transparent={true} visible={this.props.visible} onRequestClose={this.props.close}>
+                    <View style={this.styles.pageViewStyle}>
+                        <View style={this.styles.modalViewStyle}>
                             <Text style={{flex: 1, fontSize: 30}}>Hi i am a modal screen!!</Text>
                             <View style={{flex: 3, flexDirection: 'column', justifyContent: 'space-between'}}>
-                                <View style={{flexDirection: 'row', alignItems:'center', justifyContent:'space-between'}}>
+                                <View style={this.styles.optionViewStyle}>
                                     <Text style={{fontSize: 20}}>User</Text>
                                     <RadioForm initial={0} formHorizontal={true} labelHorizontal={false}
                                                radio_props={[{label: 'user1'}, {label: 'user2'}]}/>
                                 </View>
-                                <View style={{flexDirection: 'row', alignItems:'center', justifyContent:'space-between'}}>
+                                <View style={this.styles.optionViewStyle}>
                                     <Text style={{fontSize: 20}}>Action</Text>
                                     <Picker style={{height: 50, width: 100}}>
                                         <Picker.Item label="add" value="add"/>
@@ -39,11 +52,11 @@ export default class EditModal extends React.Component {
                                         <Picker.Item label="clear" value="clear"/>
                                     </Picker>
                                 </View>
-                                <View style={{flexDirection: 'row', alignItems:'center', justifyContent:'space-between'}}>
+                                <View style={this.styles.optionViewStyle}>
                                     <Text style={{fontSize: 20}}>Points</Text>
                                     <TextInput style={{backgroundColor: 'pink'}}></TextInput>
                                 </View>
-                                <View style={{flexDirection: 'row', alignItems:'center', justifyContent:'space-between'}}>
+                                <View style={this.styles.optionViewStyle}>
                                     <Text style={{fontSize: 20}}>Random Number</Text>
                                     <Switch></Switch>
                                 </View>
