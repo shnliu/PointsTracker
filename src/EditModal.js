72946ce17ec00664f443ea626b5be3220c9c22c7
Sara L <saraningliu@gmail.com>
2019-10-26 10:24:40 -0400

    finished stylesheet for edit modal

--- src/EditModal.js
+++ src/EditModal.js
@@ -16,59 +16,89 @@ export default class EditModal extends React.Component {
             flex: 1,
             backgroundColor: 'rgba(80,80,80,0.5)'
         },
-        modalViewStyle:{
+        modalViewStyle: {
             backgroundColor: '#fff',
             padding: 20,
             width: Dimensions.get('window').width * 0.9,
             height: Dimensions.get('window').height * 0.9
         },
-        optionViewStyle:{
+        optionViewStyle: {
             flexDirection: 'row',
-            alignItems:'center',
-            justifyContent:'space-between'
+            alignItems: 'center',
+            justifyContent: 'space-between'
         },
-
+        labelTextStyle: {
+            fontSize: 25
+        },
+        basicTextStyle: {
+            flex: 1,
+            fontSize: 30,
+            textDecorationLine: 'underline'
+        },
+        modalOptionStyle: {
+            flex: 1,
+            flexDirection: 'row',
+            alignItems: 'flex-end',
+            justifyContent: 'flex-end'
+        },
+        inputAreaViewStyle: {
+            flex: 3,
+            flexDirection: 'column',
+            justifyContent: 'space-between'
+        },
+        textInputStyle: {
+            height: 40,
+            borderColor: 'gray',
+            borderWidth: 1
+        },
+        modalOptionButtonStyle: {
+            fontSize: 40,
+            color: "pink",
+            margin: 5
+        },
+        pickerStyle: {
+            height: 50,
+            width: 120
+        }
     });
 
     render() {
         return (
             <View>
-                <Modal animationType="slide" transparent={true} visible={this.props.visible} onRequestClose={this.props.close}>
+                <Modal animationType="slide" transparent={true} visible={this.props.visible}
+                       onRequestClose={this.props.close}>
                     <View style={this.styles.pageViewStyle}>
                         <View style={this.styles.modalViewStyle}>
-                            <Text style={{flex: 1, fontSize: 30}}>Hi i am a modal screen!!</Text>
-                            <View style={{flex: 3, flexDirection: 'column', justifyContent: 'space-between'}}>
+                            <Text style={this.styles.basicTextStyle}>Edit Points</Text>
+                            <View style={this.styles.inputAreaViewStyle}>
                                 <View style={this.styles.optionViewStyle}>
-                                    <Text style={{fontSize: 20}}>User</Text>
+                                    <Text style={this.styles.labelTextStyle}>User</Text>
                                     <RadioForm initial={0} formHorizontal={true} labelHorizontal={false}
                                                radio_props={[{label: 'user1'}, {label: 'user2'}]}/>
                                 </View>
                                 <View style={this.styles.optionViewStyle}>
-                                    <Text style={{fontSize: 20}}>Action</Text>
-                                    <Picker style={{height: 50, width: 100}}>
-                                        <Picker.Item label="add" value="add"/>
-                                        <Picker.Item label="minus" value="subtract"/>
-                                        <Picker.Item label="transfer" value="transfer"/>
-                                        <Picker.Item label="clear" value="clear"/>
+                                    <Text style={this.styles.labelTextStyle}>Action</Text>
+                                    <Picker style={this.styles.pickerStyle}>
+                                        <Picker.Item label="PLUS" value="plus"/>
+                                        <Picker.Item label="MINUS" value="minus"/>
+                                        <Picker.Item label="TRANSFER" value="transfer"/>
+                                        <Picker.Item label="CLEAR" value="clear"/>
                                     </Picker>
                                 </View>
                                 <View style={this.styles.optionViewStyle}>
-                                    <Text style={{fontSize: 20}}>Points</Text>
-                                    <TextInput style={{backgroundColor: 'pink'}}></TextInput>
+                                    <Text style={this.styles.labelTextStyle}>Points</Text>
+                                    <TextInput style={this.styles.textInputStyle}></TextInput>
                                 </View>
                                 <View style={this.styles.optionViewStyle}>
-                                    <Text style={{fontSize: 20}}>Random Number</Text>
+                                    <Text style={this.styles.labelTextStyle}>Random Number</Text>
                                     <Switch></Switch>
                                 </View>
                             </View>
-                            <View style={{
-                                flex: 1,
-                                flexDirection: 'row',
-                                alignItems: 'flex-end',
-                                justifyContent: 'flex-end'
-                            }}>
-                                <Button style={{fontSize: 40}} onPress={this.props.close}> ✓ </Button>
-                                <Button style={{fontSize: 40}} onPress={this.props.close}> X </Button>
+                            <View style={this.styles.modalOptionStyle}>
+                                <Button style={this.styles.modalOptionButtonStyle}
+                                        onPress={this.props.close}> ✓ </Button>
+                                <Button style={this.styles.modalOptionButtonStyle}
+                                        onPress={this.props.close}> X </Button>
                             </View>
                         </View>
                     </View>
