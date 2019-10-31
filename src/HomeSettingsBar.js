0f273a2e5dec64a3d9e3312606c4c38cd1143a02
Sara L <saraningliu@gmail.com>
2019-10-30 22:25:47 -0400

    Created/updated stylesheets (for all except settings).

--- src/HomeSettingsBar.js
+++ src/HomeSettingsBar.js
@@ -1,19 +1,30 @@
 import React from 'react';
-import {View} from 'react-native';
+import {View, StyleSheet} from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
 // https://www.npmjs.com/package/react-native-vector-icons#icon-component
 
 export default class HomeSettingsBar extends React.Component {
 
-    render() {
+    styles = StyleSheet.create({
+        viewStyle: {
+            flex: 1,
+            flexDirection: 'row',
+            justifyContent: 'space-between'
+        },
+        iconStyle: {
+            fontSize: 40,
+            color: "#383838"
+        }
+    });
 
+    render() {
         return (
-            <View style={{flex: 1, flexDirection: 'row', justifyContent: 'space-between'}}>
-                <Icon name={'home'} style={{fontSize: 40, color: "#383838"}}
-                             onPress={this.props.onPressHome}> </Icon>
-                <Icon name={'settings-applications'} style={{fontSize: 40, color: "#383838"}}
-                             onPress={this.props.onPressSetting}> </Icon>
+            <View style={this.styles.viewStyle}>
+                <Icon name={'home'} style={this.styles.iconStyle}
+                      onPress={this.props.onPressHome}> </Icon>
+                <Icon name={'settings-applications'} style={this.styles.iconStyle}
+                      onPress={this.props.onPressSetting}> </Icon>
             </View>
 
         );
